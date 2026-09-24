e932377376e0002174ce6908a339c5be5df4bf2c
Christopher Watkins <chrisbwatkins@gmail.com>
2026-09-24 13:13:39 -0600

    :working on the the project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     fetchHealth(API_BASE_URL).then(setHealth);
     fetchReady(API_BASE_URL).then(setReady);
@@ -17,7 +18,7 @@ function App() {
     }, 5000);
 
     return () => clearInterval(interval);
-  }, [API_BASE_URL]);
+  }, []);
 
   const handleToggleFail = async () => {
     await toggleFail(API_BASE_URL);
